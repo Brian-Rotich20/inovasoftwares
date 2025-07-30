cd16d9df77ade67d5dc3be54e7ca9ee395c2f713
Brian-Rotich20 <ralphrotich@gmail.com>
2025-07-30 12:51:43 +0300

    removed pages

--- app/projects/[slug]/page.tsx
+++
@@ -1,3 +0,0 @@
-// Store an array of project objects with slug, title, description, etc.
-// In /projects/page.tsx, list the projects
-// In /projects/[slug]/page.tsx, use generateStaticParams to statically build each project detail page
